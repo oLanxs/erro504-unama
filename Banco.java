5c7a7a9baccf3dd08bda32d2cee9ba4fb09fa0ef
Orlando de Araújo Gonçalves <orlandoaraujo113@gmail.com>
2024-08-28 13:13:31 +0000

    testedeoperacaocomjava

---
+++ Banco.java
@@ -0,0 +1,14 @@
+class Banco{
+    public static void main(String[] args){
+        int gastosJaneiro;
+        gastosJaneiro = 15000;
+        int gastoFevereiro;
+        gastoFevereiro = 23000;
+        int gastoMarco;
+        gastoMarco = 17000;
+
+        int somatotal = gastoFevereiro + gastosJaneiro + gastoMarco;
+
+        System.out.println(somatotal);
+    }
+}
---
+++ Testeidade.java
@@ -0,0 +1,13 @@
+class Testeidade{
+    public static void main(String[] args){
+        int idade;
+        idade = 7;
+        int IdadeAnoQueVem = idade + 1;
+        double metadeidade = idade / 2.0;
+        boolean maioridade = idade >= 18;
+
+        System.out.println(IdadeAnoQueVem);
+        System.out.println(metadeidade);
+        System.out.println(maioridade);
+    }
+}
